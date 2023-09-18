75ab8a96c6f88889d201dbd4753fe08115e10e58
AnselmoNathanRocha <anselmonathanrocha16@gmail.com>
2023-09-18 04:09:00 -0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let btnMenu = document.querySelector('#menu-input');
 let btnProfile = document.querySelector('.box-profile');
+let search = document.querySelector('#search-input'); 
 let navLeft = document.querySelector('.nav-left');
 let modalProfile = document.querySelector('.modal-profile');
 
@@ -18,4 +19,11 @@ modalProfile.addEventListener('mouseover', function() {
 });
 modalProfile.addEventListener('mouseout', function() {
     modalProfile.classList.remove('visible');
+});
+
+search.addEventListener('focus', function() {
+    search.setAttribute('placeholder', 'Pesquise por produtos');
+});
+search.addEventListener('blur', function() {
+    search.removeAttribute('placeholder', 'Pesquise por produtos');
 });
