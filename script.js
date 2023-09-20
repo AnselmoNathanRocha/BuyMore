7fcf0991a9245466d486e556f99126d8f3ad3eb4
AnselmoNathanRocha <anselmonathanrocha16@gmail.com>
2023-09-20 03:21:07 -0300

    create

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+let fade = document.querySelector('#fade');
 let btnMenu = document.querySelector('#menu-input');
 let btnProfile = document.querySelector('.box-profile');
+let btnSearch = document.querySelector('#btn-search');
 let search = document.querySelector('#search-input'); 
 let navLeft = document.querySelector('.nav-left');
 let modalProfile = document.querySelector('.modal-profile');
+let load = document.querySelector('.loading');
 
 btnMenu.addEventListener('click', function() {
     navLeft.classList.toggle('hide-main');
+    fade.classList.toggle('fade');
+});
+
+btnSearch.addEventListener('click', function() {
+    fade.classList.toggle('fade-search');
+    load.classList.toggle('visible');
+        
+    function back() {
+        fade.classList.toggle('fade-search');
+        load.classList.toggle('visible');
+    }
+    setTimeout(back, 4000);
 });
 
 btnProfile.addEventListener('mouseover', function() {
