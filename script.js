4e85bb76aedfebb573e4ca9683a78743569b7d0a
AnselmoNathanRocha <anselmonathanrocha16@gmail.com>
2023-09-24 23:46:22 -0300

    new

--- script.js
+++ script.js
@@ -45,7 +45,25 @@ modalProfile.addEventListener('mouseout', function() {
 search.addEventListener('focus', function() {
     search.setAttribute('placeholder', 'Pesquise por produtos');
 });
-
 search.addEventListener('blur', function() {
     search.removeAttribute('placeholder', 'Pesquise por produtos');
 });
+
+$(function(){ 
+
+    $("#pesquisa").keyup(function(){
+      var texto = $(this).val();
+      
+      $(".card").each(function(){
+        var resultado = $(this).text().toUpperCase().indexOf(' '+texto.toUpperCase());
+        
+        if(resultado < 0) {
+          $(this).fadeOut();
+        }else {
+          $(this).fadeIn();
+        }
+      }); 
+  
+    });
+  
+  });
