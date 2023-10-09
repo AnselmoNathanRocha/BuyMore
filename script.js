680fde236fdb1ab07d329e2f3a1950f5f025f61d
AnselmoNathanRocha <anselmonathanrocha16@gmail.com>
2023-10-09 02:16:27 -0300

    update

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let modalProfile = document.querySelector('.modal-profile');
 let load = document.querySelector('.loading');
 let res = document.querySelector('.res');
 let divRes = document.querySelector('.resultado');
-let favorite = document.querySelectorAll('.favorite-on');
-let favoriteOn = document.querySelector('.imgFavorite');
+let inputFavorite = document.querySelectorAll('.favorite-on');
+let imgFavorite = document.querySelectorAll('.imgFavorite');
 
 btnMenu.addEventListener('click', function() {
     navLeft.classList.toggle('hide-main');
@@ -67,3 +67,13 @@ $(function(){
     });
   
   });
+
+  for (let i = 0; i < inputFavorite.length; i++) {
+    inputFavorite[i].addEventListener('click', function(){
+        if (inputFavorite[i].checked) {
+            imgFavorite[i].setAttribute('src', 'img/favorite-on.png');
+        } else {
+            imgFavorite[i].setAttribute('src', 'img/favorite-off.png');
+        }
+    });
+  }
